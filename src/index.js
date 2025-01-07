cc0436964b8be2357f084de44c1bf84e6c814e54
Eleni Oikonomou <oikonomou.g.eleni@outlook.com>
2025-01-06 17:14:07 -0800

    route_path

--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 //for this to be a single page application with routing using react router import the following:
-import { BrowserRouter, Routes } from "react-router-dom" /*BrowserRouter is a context provider*/
+import { BrowserRouter, Routes, Route } from "react-router-dom" /*BrowserRouter is a context provider*/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <BrowserRouter>
     <Routes>
-      <App />
+    
+      <Route path="/" element={<App/>}/> 
     </Routes>    
   </BrowserRouter>
   </React.StrictMode>
 );
+
+//path="/" (single slash) refers to homepage meaning the App is gonna be displayed on the homepage page
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
