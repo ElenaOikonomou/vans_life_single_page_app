e3c2b73c8675e28e2dc1e8df2564370b1b62c110
Eleni Oikonomou <oikonomou.g.eleni@outlook.com>
2025-01-09 17:58:42 -0800

    about_css

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom" /*BrowserRouter is a context provider*/
 import NavBar from "./components/NavBar"
 import aboutPage from "./images/aboutPage_pic.png"
+import Footer from "./components/Footer.js"
 
 
 function App() {
@@ -29,9 +30,7 @@ function Home(){
         <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
         <button>Find your van</button>
       </div>         
-      <footer className='home--footer'>
-        <p>Ⓒ 2022 #VANLIFE</p>
-       </footer>            
+      <Footer />            
     </div>
   )
 }
@@ -41,8 +40,23 @@ function About(){
   return (
     <div>
       <NavBar/>
-      <img className='about--img' src={aboutPage} alt="van_at_a_starry_night"/>
-      <h1 className='about--h1'>Don’t squeeze in a sedan when you could relax in a van.</h1>
+      <div className='about---div'>
+        <img className='about--img' src={aboutPage} alt="van_at_a_starry_night"/>      
+        <h1 className='about--h1'>Don’t squeeze in a sedan when<br/>you could relax in a van.</h1>
+        <p className='about--p'>Our mission is to enliven your road trip with the perfect travel<br/>
+        van rental. Our vans are recertified before each trip to ensure<br/>
+          your travel plans can go off without a hitch.
+        (Hitch costs extra 😉)<br/>
+        <br/>      
+        Our team is full of vanlife enthusiasts who know firsthand the<br/>
+        magic of touring the world on 4 wheels.</p>
+        <div className='about--main'>
+          <h2>Your destination is waiting.<br/>
+          Your van is ready.</h2>
+          <button>Explore our vans</button>
+        </div>
+        <Footer/>
+      </div>
     </div>
   )
 }
